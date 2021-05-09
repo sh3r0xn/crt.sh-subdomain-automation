5923d5736024827e2da3bd0d768998bcf3b371cd
sharwan s <letter2sharwan@gmail.com>
2021-05-09 13:07:04 +0530

    added output file

--- crt.sh
+++ crt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -s https://crt.sh/?q=%.$1 > /tmp/curl.out
-cat /tmp/curl.out | grep $1 | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u |sed 's/BR/ /g'|xargs -n1 
+cat /tmp/curl.out | grep $1 | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u |sed 's/BR/ /g'|xargs -n1 | tee crtsh_subdomains 
